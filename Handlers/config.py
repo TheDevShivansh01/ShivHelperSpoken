1dc48fadd3952ef4ee04f5aa35292f6baceebf5f
Shivansh Srivastava <shivansh.srivastava@teamarcs.com>
2025-08-27 15:52:26 +0530

    civic sense update

--- Handlers/config.py
+++ Handlers/config.py
@@ -3,10 +3,10 @@ from telegram import Update, PollAnswer, Poll, InlineKeyboardButton, InlineKeybo
 DIFFICULTY_MAP = {
     # Grammar
     'difficulty_synonyms': ('Data/CGL_SSC_Synonyms.xlsx', 'Synonyms'),
-    'difficulty_ssc_ows': ('Data/SSC_OneWordSubstitution.xlsx', 'Synonyms'),
+    'difficulty_ssc_ows': ('Data/SSC_OneWordSubstitution.xlsx', 'One word Substitution'),
     'difficulty_prepo': ('Data/English_Preposition.xlsx', 'Preposition'),
     'difficulty_antonyms': ('Data/Antonyms5.xlsx', 'Antonyms'),
-    'difficulty_spellcorr': ('Data/spellCorrection4old.xlsx', 'Spelling Correction'),
+    'difficulty_spellcorr': ('Data/spellCorrection4.xlsx', 'Spelling Correction'),
     'difficulty_sentcorr': ('Data/sentenceCorr4.xlsx', 'Sentence Correction'),
     'difficulty_shortIdiom': ('Data/basic_shortidiom.xlsx', 'Daily Life Idioms'),
 
@@ -21,6 +21,7 @@ DIFFICULTY_MAP = {
     'difficulty_bloodrelation': ('Data/BloodRelation.xlsx', 'Blood Relation'),
     'difficulty_bookwriter': ('Data/BookWriter.xlsx', 'Book and Writer'),
     'difficulty_IndiaGs': ('Data/India_GS.xlsx', 'About India'),
+    'difficulty_CivicSense': ('Data/CivicSense.xlsx', 'Civic Sense'),
     'difficulty_indianPolity': ('Data/IndianPolity.xlsx', 'Indian Polity'),
     'difficulty_indusvalley': ('Data/Indus_Valley.xlsx', 'Indus Valley Civilisation'),
     'difficulty_IndiaConstitute': ('Data/IndianConstitution.xlsx', 'Indian Constitution'),
@@ -42,7 +43,7 @@ DIFFICULTY_MAP = {
     'difficulty_upscpreviousyear': ('Data/UPSC_GS_2023.xlsx', 'GS-Previous years paper'),
     'difficulty_upschistory': ('Data/Upsc_history.xlsx', 'History'),
     'difficulty_upscscience': ('Data/Upsc_ScienceandTech.xlsx', 'Science and Technology'),
-    'difficulty_thehindu': ('Data/thehindu.xlsx', 'Science and Technology'),
+    'difficulty_thehindu': ('Data/thehindu.xlsx', 'The Hindu Vocab'),
 
     'difficulty_upscpresident': ('Data/IP_ThePresident.xlsx', 'The President'),
     'difficulty_upscunionandstatefunction': ('Data/IP_FnResp_ofTheUnionandStates.xlsx', ' Union and State Function and Responsibilities'),
@@ -127,7 +128,7 @@ def Topic_Kb0():
         [InlineKeyboardButton("Blood Relation", callback_data='difficulty_bloodrelation')],
         [InlineKeyboardButton("World First", callback_data='difficulty_worldtop')],
         [InlineKeyboardButton("Indian Polity", callback_data='difficulty_indianPolity')],
-        [InlineKeyboardButton("Indus Valley Civilization", callback_data='difficulty_indusvalley')],
+        [InlineKeyboardButton("Civic Sense", callback_data='difficulty_CivicSense')],
         [InlineKeyboardButton("🧑‍🦯‍➡️ Next 🧑‍🦯‍➡️", callback_data='type_topic1')]
     ]        
 
@@ -174,6 +175,8 @@ def Upsc_keyboard2():
     ] 
 def Topic_Kb2():
     return [
+        
+        [InlineKeyboardButton("Indus Valley Civilization", callback_data='difficulty_indusvalley')],
           [InlineKeyboardButton("Maurya Empire", callback_data='difficulty_Mauryaempire')],
            [InlineKeyboardButton("Pre Maurya Period", callback_data='difficulty_PreMaurya')],
            [InlineKeyboardButton("Sangam Period", callback_data='difficulty_sangamPeriod')],
@@ -237,4 +240,5 @@ ALLOWED_FILES = {
     "OddOneOut",
     "numberalphabet",
     "Sylogism",
+    "CivicSense",
 }
